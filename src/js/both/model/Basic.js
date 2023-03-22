b930de7913e87dbc823befaa9e737ce5d86eb675
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-03-22 13:16:27 +0100

    fixes

--- src/js/both/model/Basic.js
+++ src/js/both/model/Basic.js
@@ -1,77 +1,19 @@
-Ext.define("Tualo.DataSets.data.Store",{
-    extend: 'Ext.data.Store',
-    alias: 'store.tualo_datasets_json',
-  
+Ext.define('Tualo.DataSets.model.Basic', {
+    extend: 'Ext.data.Model',
     requires: [
         'Ext.data.proxy.Ajax',
         'Ext.data.reader.Json',
         'Ext.data.writer.Json'
     ],
-    constructor: function(config) {
-        config = Ext.apply({
-            proxy: {
-                type: 'ajax',
-                noCache: false,
-                api: {
-                  create  : './ds/tbl/create',
-                  read    : './ds/tbl/read',
-                  update  : './ds/tbl/update',
-                  destroy : './ds/tbl/delete'
-                },
-                listeners: {
-                  scope: this,
-                  exception: function(proxy, response, operation, eOpts) {
-                      this.fireEvent('proxyerror',response);
-                  }
-                },
-                reader: {
-                    type: 'json',
-                    rootProperty: 'data',
-                    idProperty: '__id'
-                },
-                writer: {
-                  type: 'json',
-                  writeAllFields: false,
-                  transform: {
-                    fn: function(data, request) {
-                        let result = {}, key;
-                        for (key in data) {
-                          if (
-                            data.hasOwnProperty(key) && ( 
-                              ( data[key]!=null ) ||
-                              ( this.model.getField(key).critical )
-                            )
-                          ) {
-                              result[key] = data[key];
-                          }
-                      }
-                      return result;
-                    },
-                    scope: this
-                }
-                }
-            }
-        }, config);
-  
-        this.callParent([config]);
-        
-        this.proxy.setTimeout(60000);
-        this.proxy.tablename = this.tablename;
-        this.getProxy().setApi({
-          read: './ds/'+this.tablename+'/read',
-          create: './ds/'+this.tablename+'/create',
-          update: './ds/'+this.tablename+'/update',
-          destroy: './ds/'+this.tablename+'/delete'
-        });
-  
-        //console.log(this,this.tablename);
-  
-    },
-    remoteFilter: true,
-    remoteSort: true,
-    pageSize: 1000,
-    autoLoad: false,
-    autoSync: false
-  
-  });
-  
+    
+
+    fields: [
+        {name: '__id',  type: 'string'}
+    ],
+    
+    get: function(fieldName) {
+        if (this.data.hasOwnProperty(fieldName)) return this.data[fieldName];
+        if (this.data.hasOwnProperty("__table_name") && this.data.hasOwnProperty(this.data["__table_name"]+"__"+fieldName)) return this.data[this.data["__table_name"]+"__"+fieldName];
+        return this.data[fieldName];
+    }
+});
--- src/js/both/store/Basic.js
+++ src/js/both/store/Basic.js
@@ -1,59 +1,76 @@
-Ext.define('Tualo.DataSets.store.Basic', {
-  
-    extend: 'Ext.data.Store',
-    alias: 'store.tualobasicstore',
-    type: 'json',
-    autoLoad: false,
-    autoSync: true,
-    remoteFilter: true,
-    remoteSort: true,
-    pageSize: 100,
+Ext.define("Tualo.DataSets.data.Store",{
+  extend: 'Ext.data.Store',
+  alias: 'store.tualo_datasets_json',
 
-    
-    proxy: {
-        type: 'ajax',
-        noCache: false,
-        api: {
-          read: './',
-          create: './',
-          update: './',
-          destroy: './'
-        },
-        extraParams: {
-        },
-        writer: {
-          type: 'json',
-          writeAllFields: true,
-          rootProperty: 'data'
-        },
-        reader: {
-          type: 'json',
-          rootProperty: 'data'
-        },
-        listeners: {
-          exception: function(proxy, response, operation,eopts){
-            var o,msg;
-            try{
-    
-              if (typeof response.responseJson=='object'){
-                o=response.responseJson;
-              }else{
-                o = Ext.JSON.decode(response.responseText);
+  requires: [
+      'Ext.data.proxy.Ajax',
+      'Ext.data.reader.Json',
+      'Ext.data.writer.Json'
+  ],
+  constructor: function(config) {
+      config = Ext.apply({
+          proxy: {
+              type: 'ajax',
+              noCache: false,
+              api: {
+                create  : './ds/tbl/create',
+                read    : './ds/tbl/read',
+                update  : './ds/tbl/update',
+                destroy : './ds/tbl/delete'
+              },
+              listeners: {
+                scope: this,
+                exception: function(proxy, response, operation, eOpts) {
+                    this.fireEvent('proxyerror',response);
+                }
+              },
+              reader: {
+                  type: 'json',
+                  rootProperty: 'data',
+                  idProperty: '__id'
+              },
+              writer: {
+                type: 'json',
+                writeAllFields: false,
+                transform: {
+                  fn: function(data, request) {
+                      let result = {}, key;
+                      for (key in data) {
+                        if (
+                          data.hasOwnProperty(key) && ( 
+                            ( data[key]!=null ) ||
+                            ( this.model.getField(key).critical )
+                          )
+                        ) {
+                            result[key] = data[key];
+                        }
+                    }
+                    return result;
+                  },
+                  scope: this
               }
-              
-              msg = o.msg;
-              Ext.toast({
-               html: msg,
-               title: 'Fehler ('+proxy.tablename+')',
-               width: 400,
-               align: 't'
-             });
-    
-            }catch(e){
-              msg = response.responseText;
-            }
-            operation.setException(msg);
+              }
           }
-        }
-      }
-})  
+      }, config);
+
+      this.callParent([config]);
+      
+      this.proxy.setTimeout(60000);
+      this.proxy.tablename = this.tablename;
+      this.getProxy().setApi({
+        read: './ds/'+this.tablename+'/read',
+        create: './ds/'+this.tablename+'/create',
+        update: './ds/'+this.tablename+'/update',
+        destroy: './ds/'+this.tablename+'/delete'
+      });
+
+      //console.log(this,this.tablename);
+
+  },
+  remoteFilter: true,
+  remoteSort: true,
+  pageSize: 1000,
+  autoLoad: false,
+  autoSync: false
+
+});
