5d1ff284a3d83bb22e3f703e976726f6ebfe440f
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-08-10 19:32:12 +0200

    add batch

---
+++ src/js/both/commands/DsBatchChanges.js
@@ -0,0 +1,47 @@
+Ext.define('Ext.cmp.cmp_ds.views.DSBatchChanges', {
+    statics: {
+      glyph: 'magic',
+      title: 'Stapeländerungen',
+      tooltip: 'Stapeländerungen'
+    },
+    extend: 'Ext.panel.Panel',
+    alias: 'widget.ds_batch_command',
+    layout: 'fit',
+    items: [
+      {
+        xtype: 'form',
+        itemId: 'ds_batch_commandform',
+        bodyPadding: '25px',
+        items: [
+          {
+            xtype: 'label',
+            text: 'Durch Klicken auf *Anwenden* werden die Änderungen auf alle Datensätze der aktuellen Liste angewendet',
+          }
+        ]
+      }
+    ],
+    loadRecord: function (record, records, selectedrecords, view) {
+      this.record = record;
+      this.records = records;
+      this.selectedrecords = selectedrecords;
+      this.view = view;
+      console.log('loadRecord', arguments);
+    },
+    getNextText: function () {
+      return 'Anwenden';
+    },
+    run: async function () {
+      let parent = Ext.getCmp(this.calleeId);
+      
+      this.records.forEach((record)=>{
+        if (record.id != this.record.id){
+            for( var key in this.record.modified){
+
+                record.set(key,this.record.get(key));
+            }
+        }
+      });
+      return null;
+    }
+  });
+  
