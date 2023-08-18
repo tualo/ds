1932ec86c76b5667897a205be5f9eed3c0f1bf9e
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-08-18 14:35:57 +0200

    fix checksums

--- src/Checks/StoredProcedures.php
+++ src/Checks/StoredProcedures.php
@@ -29,15 +29,15 @@ class StoredProcedures extends PostCheck {
 
         $def = [
             'dsx_rest_api_unescape'=>'3c1a32b3abe38f0766fc492abdf90e11',
-            'dsx_filter_proc'=>'a5fcad4fb7ac60a5cead96de31794ac4',
+            'dsx_filter_proc'=>'c20b23d28ad075003f9815d923504b32',
             'dsx_read_order'=>'438775628deb3dc4a83c238f1f2be89e',
-            'create_or_upgrade_hstr_table'=>'ad429a9b1ae27217a536bede47edaf2e',
+            'create_or_upgrade_hstr_table'=>'8cc88ded3a0bb2fb248854a0f7c35a6e',
             'dsx_filter_operator'=>'8841e1840ec0dff0a44c1ea43020687e',
             'dsx_get_key_sql'=>'3275f6c90b641c0fc49d85e747342d89',
-            'dsx_filter_term'=>'54352cc2aef440d4d30e111106907d51',
+            'dsx_filter_term'=>'04dd87506bf5b3c465bd249e49053fac',
             'dsx_filter_values_extract'=>'302f4d9815bb7b237bf32afb75c522db',
-            'dsx_rest_api_get'=>'c79762e9ebdf6cd607e61086cfa681bc',
-            'dsx_rest_api_set'=>'3f4801efaf0b1ab662f618d48207628a'
+            'dsx_rest_api_get'=>'0a0049d380ef45a1f59e942d087ec9aa',
+            'dsx_rest_api_set'=>'185387ea72f076fd4a872bdb1572d799'
         ];
         self::procedureCheck('ds',$def);
         
