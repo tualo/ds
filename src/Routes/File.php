9f2acb1272cf2ed70093ab56c5e8de030d150741
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-05 11:32:04 +0200

    add file routes

--- src/Routes/File.php
+++ src/Routes/File.php
@@ -10,6 +10,39 @@ use Tualo\Office\DS\DSFileHelper;
 
 class File implements IRoute{
     public static function register(){
+
+        Route::add('/dsfile/download',function(){
+
+            $db = App::get('session')->getDB();
+            $session = App::get('session');
+            try {
+                
+                $tablename = $_REQUEST['t'];
+                $id = intval($_REQUEST['id']);
+                $direct=false;
+                $base64=false;
+                $maxwidth=-1;
+                $maxheight=-1;
+                $result = DSFileHelper::getFile( $db,$tablename,$id, $direct,$base64,$maxwidth,$maxheight );
+                foreach($result as $k=>$v){
+                    App::result($k,$v);
+                }
+                
+            }catch(\Exception $e){
+                App::result('msg', $e->getMessage());
+            }
+            App::contenttype('application/json');
+        
+        },array('get','post'),true);
+
+        Route::add('/dsfile/upload',function(){
+            DSFileHelper::uploadRoute($tablename = $_REQUEST['t']);
+        },array('get','post'),true);
+
+        Route::add('/dsfile/(?P<tablename>[\w\-\_\|]+)/upload',function($matches){
+            DSFileHelper::uploadRoute($tablename = $matches['tablename']);
+        });
+
         Route::add('/dsfile/mime',function(){
 
             $db = App::get('session')->getDB();
