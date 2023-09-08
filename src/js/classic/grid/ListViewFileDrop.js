46ca785e7d3cddbe10ead7fe446d1e17f3296d4c
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-09-08 13:55:58 +0200

    add filedrop list

---
+++ src/js/classic/grid/ListViewFileDrop.js
@@ -0,0 +1,78 @@
+
+Ext.define('Tualo.DataSets.ListViewFileDrop',  {
+    extend: 'Tualo.DataSets.ListView',
+    alias: 'widget.dslistviewfiledrop',
+
+    listeners: {
+
+        drop: {
+            element: 'el',
+            fn: 'onDrop'
+        },
+
+        dragstart: {
+            element: 'el',
+            fn: 'addDropZone'
+        },
+
+        dragenter: {
+            element: 'el',
+            fn: 'addDropZone'
+        },
+
+        dragover: {
+            element: 'el',
+            fn: 'addDropZone'
+        },
+
+        dragleave: {
+            element: 'el',
+            fn: 'removeDropZone'
+        },
+
+        dragexit: {
+            element: 'el',
+            fn: 'removeDropZone'
+        },
+
+    },
+
+    plugins: [
+        {
+            ptype: 'cellediting',
+            clicksToEdit: 1,
+            pluginId: 'celleditingplugin',
+            listeners: {
+                edit: function(editor, fld){
+                    console.log('edit',fld);
+                    this.grid.checkAutoNewRow(fld.rowIdx);
+                    this.grid.fireEvent('edited',fld.record,fld);
+                    return true;
+                }
+            }
+        },{
+            ptype: 'gridexporter'
+        },{
+            ptype: 'defaultfilters',
+            pluginId: 'gridfiltersplugin',
+            menuFilterText: 'Filter'
+        },
+        {
+            ptype:'tualoclipboard'
+        }
+    ],
+    initComponent: function(){
+        this.callParent(arguments);
+        Ext.defer(this.checkAutoNewRow,100,this);
+    },
+    checkAutoNewRow: function(rowIndex){
+        if(Ext.getApplication().getDebug()===true) console.log('checkAutoNewRow',rowIndex);
+        let doNotAdd = false;
+        let store = this.getStore();
+        if (typeof rowIndex=='undefined') rowIndex=-1;
+        if ((doNotAdd===false)&&(rowIndex===store.getCount()-1)){
+            store.add({});
+        }
+        return true;
+    }
+});
