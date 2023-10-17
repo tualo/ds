52ce83d8df839facde2963f861b2dda11011a69d
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-17 12:28:57 +0200

    disable error on demand

--- src/DSFiles.php
+++ src/DSFiles.php
@@ -6,31 +6,45 @@ use Tualo\Office\DS\DSTable;
 class DSFiles {
     private mixed $db;
     private string $tablename;
+    private string $error = '';
     public static function instance(string $tablename='test'):DSFiles{
         return new DSFiles( App::get('session')->getDB(),$tablename);
+    }
+    function getError():string{
+        return $this->error;
     }
     function __construct(mixed $db,string $tablename){
         $this->db=$db;
         $this->tablename=$tablename;
     }
 
-    // php:
+    
+    // :php
     //    $logodata = \Tualo\Office\DS\DSFiles::instance('tualocms_bilder')->getBase64('titel','Wahllogo'));
 
-    public function getBase64(string $fieldName,string $fieldValue):string{
+    public function getBase64(string $fieldName,string $fieldValue,bool $emptyOnError=false):string{
 
         $table = new DSTable($this->db ,$this->tablename);
         $table->filter($fieldName,'=',$fieldValue);
         $table->read();
-        if ($table->empty()) throw new \Exception('File not found!');
+        if ($table->empty()){
+            $this->error = 'File not found! (in '.$this->tablename.')';
+            if ($emptyOnError) return '';
+            throw new \Exception('File not found!');
+        }
 
         $record = $table->getSingle();
         $record['tablename'] = $this->tablename;
 
-        if (($mime = $this->db->singleValue("select type from ds_files where file_id = {file_id} and table_name= {tablename}",$record,'type'))===false){
+        if (($mime = $this->db->singleValue("select type from ds_files where file_id = {__file_id} and table_name= {tablename}",$record,'type'))===false){
+
+            $this->error = 'File not found! (in ds_files)';
+            if ($emptyOnError) return '';
             throw new \Exception('File not found!');
         }
-        if (($dbcontent = $this->db->singleValue("select data from ds_files_data where file_id = {file_id}  ",$record,'data'))===false){
+        if (($dbcontent = $this->db->singleValue("select data from ds_files_data where file_id = {__file_id}  ",$record,'data'))===false){
+            $this->error = 'File not found! (in ds_files_data)';
+            if ($emptyOnError) return '';
             throw new \Exception('File not found!');
         }
         return $dbcontent;
