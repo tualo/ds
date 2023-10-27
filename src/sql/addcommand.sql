e7684cef4c4693c8e2d614efd378d06121df9d97
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2023-10-27 09:00:56 +0200

    fix missed primary flag

--- src/sql/addcommand.sql
+++ src/sql/addcommand.sql
@@ -2,3 +2,9 @@ delimiter ;
 insert ignore into ds_addcommands_xtypes (id,name) values ('compiler_command','Kompiler');
 insert ignore into ds_addcommands_xtypes (id,name) values ('ds_batch_command','Batchupdate');
 insert ignore into ds_addcommands_xtypes (id,name) values ('ds_refresh_information_schema_command','DDL-Refresh');
+
+
+
+update `ds_column` set `is_primary`=1 where 
+`table_name`='view_ds_listfilters' and `column_name`='id'
+and (select sum(`is_primary`) from `ds_column` where `table_name`='view_ds_listfilters')=0;
