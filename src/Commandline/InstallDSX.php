8e4a6d7edbc80683678740835a07639c2bd7ccdf
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2024-08-07 16:09:33 +0200

    fix install order

--- src/Commandline/InstallDSX.php
+++ src/Commandline/InstallDSX.php
@@ -7,9 +7,9 @@ class InstallDSX extends CommandLineInstallSQL  implements ICommandline{
     public static function getDir():string {   return dirname(__DIR__,1); }
     public static $shortName  = 'ds-dsx';
     public static $files = [
+        'dsx/000.dsx_get_key_sql' => 'setup 000.dsx_get_key_sql ',
         'dsx/000.dsx_filter_values' => 'setup 000.dsx_filter_values ',
         'dsx/000.dsx_filter_values_extract' => 'setup 000.dsx_filter_values_extract ',
-        'dsx/000.dsx_get_key_sql' => 'setup 000.dsx_get_key_sql ',
         'dsx/001.dsx_filter_operator' => 'setup 001.dsx_filter_operator ',
         'dsx/create_or_upgrade_hstr_table' => 'setup create_or_upgrade_hstr_table ',
         'dsx/dsx_read_order' => 'setup dsx_read_order ',
