20c12154820c33c5c798f952edc16816324c60ac
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-03-20 14:54:45 +0100

    x

--- src/DSTable.php
+++ src/DSTable.php
@@ -247,6 +247,7 @@ class DSTable
                 )
             ]);
 
+
             //$this->db->direct('set @request = {d}', [ 'd'=> $this->requestData($input,['type'=>'insert','update'=>true]) ]);
             return $this->dsx_rest_api_set();
         } catch (\Exception $e) {
