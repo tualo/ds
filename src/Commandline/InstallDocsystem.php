b06a19937eb1594a3bfcd025c1fd6ce41b31e633
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-08-11 09:21:26 +0200

    add docsystem ds

--- src/Commandline/InstallDocsystem.php
+++ src/Commandline/InstallDocsystem.php
@@ -1,24 +1,33 @@
 <?php
+
 namespace Tualo\Office\DS\Commandline;
+
 use Tualo\Office\Basic\ICommandline;
 use Tualo\Office\Basic\CommandLineInstallSQL;
 
-class InstallDocsystem extends CommandLineInstallSQL  implements ICommandline{
-    public static function getDir():string {   return dirname(__DIR__,1); }
+class InstallDocsystem extends CommandLineInstallSQL  implements ICommandline
+{
+    public static function getDir(): string
+    {
+        return dirname(__DIR__, 1);
+    }
     public static $shortName  = 'ds-docsystem';
     public static $files = [
 
-        'docsystem/docsystem_ds'=> 'setup docsystem_ds ',
-        'docsystem/docsystem_ds_column'=> 'setup docsystem_ds_column ',
-        'docsystem/docsystem_groups'=> 'setup docsystem_groups ',
-        
-        'docsystem/view_docsystem_ds_access'=> 'setup view_docsystem_ds_access ',
-        'docsystem/view_docsystem_ds_column_documentation'=> 'setup view_docsystem_ds_column_documentation ',
-        'docsystem/view_docsystem_ds_ds_reference_tables'=> 'setup view_docsystem_ds_ds_reference_tables ',
-        'docsystem/view_docsystem_ds_ds_reference_tables_used_ds'=> 'setup view_docsystem_ds_ds_reference_tables_used_ds ',
-        
-        'docsystem/pug'=> 'setup pug templates ',
+        'docsystem/docsystem_ds' => 'setup docsystem_ds ',
+        'docsystem/docsystem_ds_column' => 'setup docsystem_ds_column ',
+        'docsystem/docsystem_groups' => 'setup docsystem_groups ',
+
+        'install/ds/docsystem_ds.ds' => 'setup docsystem_ds ',
+        'install/ds/docsystem_ds_column.ds' => 'setup docsystem_ds_column ',
+        'install/ds/docsystem_groups.ds' => 'setup docsystem_groups ',
+
+        'docsystem/view_docsystem_ds_access' => 'setup view_docsystem_ds_access ',
+        'docsystem/view_docsystem_ds_column_documentation' => 'setup view_docsystem_ds_column_documentation ',
+        'docsystem/view_docsystem_ds_ds_reference_tables' => 'setup view_docsystem_ds_ds_reference_tables ',
+        'docsystem/view_docsystem_ds_ds_reference_tables_used_ds' => 'setup view_docsystem_ds_ds_reference_tables_used_ds ',
+
+        'docsystem/pug' => 'setup pug templates ',
 
     ];
-    
 }
