799a44236797fac32186456b7cbfff105e3eabe5
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-09-29 14:03:46 +0200

    Version bump to 1.4.16

--- src/Commandline/InstallMain.php
+++ src/Commandline/InstallMain.php
@@ -43,6 +43,9 @@ class InstallMain extends CommandLineInstallSQL  implements ICommandline
         'install/ds/ds_listroutes' => 'setup ds_listroutes', // quickfix
         'main/500-ui/050-list/054.view_ds_list' => 'setup 054.view_ds_list ',
         'main/500-ui/050-list/view_dd_column' => 'setup view_dd_column ',
+
+        'install/ds/ds_form_groups' => 'setup ds_form_groups', // quickfix
+
         'main/500-ui/060-form/064.view_ds_formfieldgroups' => 'setup 064.view_ds_formfieldgroups ',
         'main/500-ui/060-form/065.view_ds_formfields' => 'setup 065.view_ds_formfields ',
         'main/500-ui/060-form/066.view_ds_form' => 'setup 066.view_ds_form ',
