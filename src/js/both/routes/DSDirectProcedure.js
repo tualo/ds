650e7311364f674ba54817d2ec206c52682a9194
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-05-22 18:37:49 +0200

    Version bump to 1.8.50

--- src/js/both/routes/DSDirectProcedure.js
+++ src/js/both/routes/DSDirectProcedure.js
@@ -21,7 +21,7 @@ Ext.define('Tualo.routes.ds.DSDirectProcedure', {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        list: Ext.JSON.encode([{ id: values.id }])
+                        list: [{ id: values.id }]
                     })
                 });
                 let json = await res.json();
