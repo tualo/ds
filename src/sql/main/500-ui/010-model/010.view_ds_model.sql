ea573e4aaffb7e2abbb9ccf5fd130c82b59435dd
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2026-09-15 09:12:28 +0200

    Version bump to 1.8.139

--- src/sql/main/500-ui/010-model/010.view_ds_model.sql
+++ src/sql/main/500-ui/010-model/010.view_ds_model.sql
@@ -1,5 +1,24 @@
 delimiter;
 
+CREATE TABLE IF NOT EXISTS `ds_additional_columns` (
+  `table_name` varchar(128) NOT NULL,
+  `column_name` varchar(64) NOT NULL,
+  `sql_command` text DEFAULT NULL,
+  `checked` tinyint(4) DEFAULT 0,
+  `error_message` varchar(255) DEFAULT '',
+  PRIMARY KEY (`table_name`,`column_name`),
+  CONSTRAINT `fk_ds_additional_columns_ds` FOREIGN KEY (`table_name`) REFERENCES `ds` (`table_name`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS `ds_virtual_columns` (
+  `table_name` varchar(128) NOT NULL,
+  `column_name` varchar(64) NOT NULL,
+  `checked` tinyint(4) DEFAULT 0,
+  `type` varchar(128) NOT NULL,
+  PRIMARY KEY (`table_name`,`column_name`),
+  CONSTRAINT `fk_ds_virtual_columns_ds` FOREIGN KEY (`table_name`) REFERENCES `ds` (`table_name`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
 call addfieldifnotexists(
     'ds',
     'modelbaseclass',
